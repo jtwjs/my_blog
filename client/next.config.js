8b0708d0c6b72b9d164589435df62c8ff00b39b5
jtw <xodnd9503@gmail.com>
2022-03-06 23:45:50 +0900

    [#3]Chore: assets 별칭 경로 추가

--- client/next.config.js
+++ client/next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
         "@hooks": path.resolve(__dirname, "src/hooks"),
         "@components": path.resolve(__dirname, "src/components"),
         "@styles": path.resolve(__dirname, "src/styles"),
+        "@assets": path.resolve(__dirname, "src/assets"),
       },
       ...config.resolve,
     };
