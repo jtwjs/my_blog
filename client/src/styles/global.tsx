d8f669894aeb9ad8b0baadabe78e2f1f99f47274
jtw <xodnd9503@gmail.com>
2022-03-06 23:45:16 +0900

    [#3]Style: 글로벌 스타일 추가

--- client/src/styles/global.tsx
+++ client/src/styles/global.tsx
@@ -1,10 +1,67 @@
-import { Global, css } from "@emotion/react";
+import { Global, css, Theme } from "@emotion/react";
 
-const style = css`
+const style = (theme: Theme) => css`
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: "Noto Sans KR", sans-serif;
+    color: ${theme.color.text_base};
+  }
+
+  input,
+  button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+
+  button {
+    font-family: "Noto Sans KR", sans-serif;
+    cursor: pointer;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  [tabindex]:focus-visible,
+  label:focus-visible,
+  button:focus-visible,
+  input:focus-visible {
+    outline: none;
+    box-shadow: 0 0 0 3px rgba(255, 255, 255, 0.65);
+  }
+
+  .a11y {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border: 0;
   }
 `;
 
