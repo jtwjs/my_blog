050e1aeddaaf2cb56eb61623cd1b5174247e1a7f
jtw <xodnd9503@gmail.com>
2022-03-06 23:45:00 +0900

    [#3]Chore: 파비콘 및 구글폰트 적용

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -5,7 +5,20 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          <title>JTW Blog</title>
+        </Head>
         <body>
           <Main />
           <NextScript />
