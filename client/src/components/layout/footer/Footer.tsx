43a286dc4337cf65937591c5805426b412240308
jtw <xodnd9503@gmail.com>
2022-03-10 23:25:02 +0900

    [#3]Feat: Footer 추가

---
+++ client/src/components/layout/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+import * as S from "./Footer.style";
+
+const Footer: FC = () => {
+  return (
+    <footer css={S.wrapper}>
+      <span>Copyright © 2022 Woong</span>
+    </footer>
+  );
+};
+
+export default Footer;
