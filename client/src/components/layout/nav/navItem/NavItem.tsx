9d978aa6b51084de2042d335998e2031778ce36e
jtw <xodnd9503@gmail.com>
2022-03-12 22:10:56 +0900

    [#3]Feat: current page에 맞는 메뉴 css, aria 속성 추가

--- client/src/components/layout/nav/navItem/NavItem.tsx
+++ client/src/components/layout/nav/navItem/NavItem.tsx
@@ -1,28 +1,37 @@
 import React, { FC } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
+import { navItemInterface } from "@assets/static/nav";
 import * as S from "./NavItem.style";
 
-type NavItemProps = {
-  link: string;
-  label: string;
-  external: boolean;
-};
+const NavItem: FC<navItemInterface> = ({ path, label, external }) => {
+  const router = useRouter();
+  const currentPage = router.pathname === path;
 
-const NavItem: FC<NavItemProps> = ({ link, label, external }) => (
-  <li css={S.wrapper}>
-    {external ? (
-      <a css={S.link} href={link} target="_blank" rel="noopener">
-        <span>{label}</span>
-      </a>
-    ) : (
-      <Link href={link}>
-        <a css={S.link}>
+  return (
+    <li css={S.wrapper}>
+      {external ? (
+        <a
+          css={(theme) => S.link(theme, { isActive: currentPage })}
+          href={path}
+          target="_blank"
+          rel="noopener"
+        >
           <span>{label}</span>
         </a>
-      </Link>
-    )}
-  </li>
-);
+      ) : (
+        <Link href={path}>
+          <a
+            css={(theme) => S.link(theme, { isActive: currentPage })}
+            aria-current={currentPage ? "page" : "false"}
+          >
+            <span>{label}</span>
+          </a>
+        </Link>
+      )}
+    </li>
+  );
+};
 
 export default NavItem;
