a1f37889238f990cf21ae29d83f0f505f2d3e806
jtw <xodnd9503@gmail.com>
2022-03-13 17:02:29 +0900

    [#7]Feat: about 페이지 메뉴 추가

--- client/src/assets/static/nav.ts
+++ client/src/assets/static/nav.ts
@@ -8,6 +8,7 @@ export const navItems: navItemInterface[] = [
   { label: "home", path: "/home" },
   { label: "blog", path: "/blog" },
   { label: "calendar", path: "/calendar" },
+  { label: "about", path: "/about" },
   { label: "guest", path: "/guest" },
   { label: "git", path: "https://github.com/jtwjs", external: true },
 ];
