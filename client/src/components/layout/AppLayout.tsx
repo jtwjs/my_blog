56de788411c72feb9a04a28e48c3ca974867aa5e
jtw <xodnd9503@gmail.com>
2022-03-13 17:00:32 +0900

    [#7]Feat: useTheme 커스텀훅 추가 및 적용, 프로바이더 구조 변경

--- client/src/components/layout/AppLayout.tsx
+++ client/src/components/layout/AppLayout.tsx
@@ -1,23 +1,29 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import { ThemeProvider } from "@emotion/react";
 
+import useTheme from "@hooks/useTheme";
 import useCloseAside from "./hooks/useCloseAside";
 import Header from "./header/Header";
 import Aside from "./aside/Aside";
 import Footer from "./footer/Footer";
 import * as S from "./AppLayout.style";
+import globalStyles from "@styles/global";
+import Theme from "@styles/theme";
 
 const AppLayout: FC = ({ children }) => {
+  const [theme] = useTheme();
   const { asideRef, isShowAside, exceptRef } = useCloseAside();
 
   return (
-    <>
+    <ThemeProvider theme={Theme[theme]}>
+      {globalStyles}
       <Header ref={exceptRef} />
       <Aside ref={asideRef} isShowAside={isShowAside} />
       <main css={S.main}>
         <section css={S.container}>{children}</section>
       </main>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 
