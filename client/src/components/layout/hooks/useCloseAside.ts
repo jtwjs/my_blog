ff1e1d6f897a056e9ec3fafe3f7aa5f12d600058
jtw <xodnd9503@gmail.com>
2022-03-16 23:06:10 +0900

    [#10]Feat: 페이지 이동시 사이드바 닫힘 적용

--- client/src/components/layout/hooks/useCloseAside.ts
+++ client/src/components/layout/hooks/useCloseAside.ts
@@ -1,18 +1,27 @@
-import { useRef, useCallback } from "react";
+import { useRef, useCallback, useEffect } from "react";
+import { useRouter } from "next/router";
 import { useRecoilState } from "recoil";
 
 import asideToggleAtom from "@recoil/asideToggle";
 import useOnClickOutside from "@hooks/useOnClickOutside";
 
 const useCloseAside = () => {
+  const router = useRouter();
+
   const asideRef = useRef(null);
   const exceptRef = useRef(null);
+
   const [isShowAside, setIsShowAside] = useRecoilState(asideToggleAtom);
 
   const handleHideAside = useCallback(() => {
     setIsShowAside(false);
   }, []);
+
   useOnClickOutside(asideRef, handleHideAside, exceptRef);
+
+  useEffect(() => {
+    setIsShowAside(false);
+  }, [router.pathname]);
 
   return { asideRef, isShowAside, exceptRef };
 };
