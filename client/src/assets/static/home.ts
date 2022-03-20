46a365f8f210965ff262c509c350e84fe3be5d7f
jtw <xodnd9503@gmail.com>
2022-03-20 19:14:01 +0900

    [#10]Feat: calendar static 파일 추가, 메뉴 데이터 변경

--- client/src/assets/static/home.ts
+++ client/src/assets/static/home.ts
@@ -1,9 +1,9 @@
-interface featureItemsInterface {
+interface FeatureItem {
   title: string;
   desc: string;
 }
 
-export const featureItems: featureItemsInterface[] = [
+export const featureItems: FeatureItem[] = [
   {
     title: "daily blog",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
