a8e0fdbaaf6398bdedbf9a7512f64c586ea7f2a1
jtw <xodnd9503@gmail.com>
2022-03-20 20:07:30 +0900

    [#12]Fix: 현재 달력에 이전 달의 날짜가 잘못표시된 이슈 수정

--- client/src/types/calendar.ts
+++ client/src/types/calendar.ts
@@ -2,6 +2,9 @@ import dayjs from "dayjs";
 
 export interface MonthYear {
   startDate: dayjs.Dayjs;
+  prevMonthStartDate: dayjs.Dayjs;
+  nextMonthStartDate: dayjs.Dayjs;
+  firstWeekPrevMonthDate: dayjs.Dayjs;
   firstDOW: number;
   lastDate: number;
   prevMonthLastDate: number;
