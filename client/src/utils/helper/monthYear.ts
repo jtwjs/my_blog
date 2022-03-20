1653a2c97ab658535ee1583f47015ee706374412
jtw <xodnd9503@gmail.com>
2022-03-20 20:00:27 +0900

    [#10]Fix: 현재 달력에 이전 달의 날짜가 잘못표시된 이슈 수정

--- client/src/utils/helper/monthYear.ts
+++ client/src/utils/helper/monthYear.ts
@@ -15,15 +15,23 @@ export const getMonthYearDetails = (initialDate: dayjs.Dayjs): MonthYear => {
   const date = initialDate.format("DD");
   const dayOfWeek = initialDate.format("dddd");
   const startDate = dayjs(`${year}${month}01`);
+  const prevMonthStartDate = startDate.clone().subtract(1, "month");
+  const nextMonthStartDate = startDate.clone().add(1, "month");
   const firstDOW = Number(startDate.format("d"));
   const lastDate = Number(startDate.clone().endOf("month").format("DD"));
   const monthName = startDate.format("MMMM");
   const prevMonthLastDate = Number(
-    startDate.clone().subtract(1, "month").endOf("month").format("DD")
+    prevMonthStartDate.endOf("month").format("DD")
+  );
+  const firstWeekPrevMonthDate = prevMonthStartDate.set(
+    "date",
+    prevMonthLastDate - firstDOW + 1
   );
 
   return {
     startDate,
+    prevMonthStartDate,
+    nextMonthStartDate,
     firstDOW,
     lastDate,
     monthName,
@@ -32,6 +40,7 @@ export const getMonthYearDetails = (initialDate: dayjs.Dayjs): MonthYear => {
     month,
     year,
     prevMonthLastDate,
+    firstWeekPrevMonthDate,
   };
 };
 
