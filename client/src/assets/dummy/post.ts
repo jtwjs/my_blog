841e099bfe3ead3479f6a0fbc6e2588fc8ea7134
jtw <xodnd9503@gmail.com>
2022-04-19 23:09:09 +0900

    [#19]Feat: postCard 인터페이스, 더미데이터 추가

---
+++ client/src/assets/dummy/post.ts
@@ -0,0 +1,40 @@
+import { IPostCard } from "@type/blog";
+import dayjs from "dayjs";
+
+export const dummyPostCards: IPostCard[] = [
+  {
+    id: "0",
+    thumbnail: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    createAt: dayjs("2022-04-19").format("MMMM DD, YYYY"),
+    title: "웹 접근성이란?",
+    desc: "접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든! 접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든!",
+  },
+  {
+    id: "1",
+    thumbnail: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    createAt: dayjs("2022-04-19").format("MMMM DD, YYYY"),
+    title: "웹 접근성이란?",
+    desc: "접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든! 접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든!",
+  },
+  {
+    id: "2",
+    thumbnail: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    createAt: dayjs("2022-04-19").format("MMMM DD, YYYY"),
+    title: "웹 접근성이란?",
+    desc: "접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든! 접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든!",
+  },
+  {
+    id: "3",
+    thumbnail: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    createAt: dayjs("2022-04-19").format("MMMM DD, YYYY"),
+    title: "웹 접근성이란?",
+    desc: "접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든! 접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든!",
+  },
+  {
+    id: "4",
+    thumbnail: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    createAt: dayjs("2022-04-19").format("MMMM DD, YYYY"),
+    title: "웹 접근성이란?",
+    desc: "접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든! 접근성은 아주 중요하거든!접근성은 아주 중요하거든!접근성은 아주 중요하거든!",
+  },
+];
