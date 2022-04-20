c88e8da414365754ae572caadd150db296504c3f
jtw <xodnd9503@gmail.com>
2022-04-20 23:56:49 +0900

    [#19]Feat: forwardRef 추가

--- client/src/components/post/PostCard.tsx
+++ client/src/components/post/PostCard.tsx
@@ -1,22 +1,24 @@
-import { FC } from "react";
+import { FC, forwardRef, Ref } from "react";
 
 import { IPostCard } from "@type/blog";
 import * as S from "./PostCard.style";
 
-type PostCardProps = Omit<IPostCard, "id">;
-const PostCard: FC<PostCardProps> = ({ thumbnail, createAt, title, desc }) => {
-  return (
-    <article css={S.post}>
-      <figure css={S.thumbnail}>
-        <img src={thumbnail} alt="" />
-      </figure>
-      <div css={S.description}>
-        <time>{createAt}</time>
-        <h4>{title}</h4>
-        <p>{desc}</p>
-      </div>
-    </article>
-  );
-};
+type PostCardProps = Omit<IPostCard, "id"> & { ref: Ref<HTMLDivElement> };
+const PostCard: FC<PostCardProps> = forwardRef(
+  ({ thumbnail, createAt, title, desc }, ref: Ref<HTMLDivElement>) => {
+    return (
+      <article css={S.post} ref={ref}>
+        <figure css={S.thumbnail}>
+          <img src={thumbnail} alt="" />
+        </figure>
+        <div css={S.description}>
+          <time>{createAt}</time>
+          <h4>{title}</h4>
+          <p>{desc}</p>
+        </div>
+      </article>
+    );
+  }
+);
 
 export default PostCard;
